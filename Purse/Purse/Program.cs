1440f39f001f685182f3bc0ace3a7eef56ade907
Volodya Mykhailiuk <volodymyr.mykhailiuk.pm.2012@lpnu.ua>
2017-05-21 19:32:43 +0300

    first commit

---
+++ Purse/Purse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = new Money.Money(1000);
+            Console.WriteLine(money.GetMoney());
+            var credit = new CreditCard.CreditCard(500);
+            Console.WriteLine(credit.GetSumCreditCard());
+            var bcard = new BusinessCard.BusinessCard("Vova");
+            Console.WriteLine(bcard.FindOutWhose());
+            Console.WriteLine("This is working");
+        }
+    }
+}
---
+++ Purse/Purse/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Purse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Purse")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1482528b-9b0b-4f3f-95ec-3358217d40c7")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
